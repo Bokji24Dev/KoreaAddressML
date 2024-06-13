922ae7a23491e76bc05dee0477a7cb0d6e69de01
Mineru98 <rmstjr1030@naver.com>
2024-06-13 12:20:59 +0000

    Fix : 학습 코드 수정

--- koraddr/preprocessing.py
+++ koraddr/preprocessing.py
@@ -1,29 +1,30 @@
 # -*- coding:utf-8 -*-
-import orjson
 import pickle
 import warnings
 from tqdm import tqdm
 from typing import List
 from transformers import AutoTokenizer
 
-warnings.filterwarnings('ignore')
+warnings.filterwarnings("ignore")
 blank_token = 29528
+
 
 def process_string(s):
     # 문자열을 공백을 기준으로 분리
     words = s.split()
-    
+
     # 단어의 개수가 1개 이하인 경우 변환 없이 반환
     if len(words) <= 1:
         return s
-    
+
     # 마지막 두 단어가 같은 경우
     if words[-1] == words[-2]:
         # 마지막 단어를 제외하고 나머지를 재조합
-        return ' '.join(words[:-1])
+        return " ".join(words[:-1])
     else:
         # 두 단어가 같지 않다면 원래 문자열 반환
         return s
+
 
 def pad_list_to_32(data: List[int], padding_value=blank_token):
     while len(data) < 32:
@@ -47,12 +48,12 @@ for i, row in tqdm(enumerate(rows), total=len(rows)):
         check_sum = False
     if len(texts) == 1024:
         tokens = tokenizer.batch_encode_plus(texts)
-        for j, ids in enumerate(tokens['input_ids']):
+        for j, ids in enumerate(tokens["input_ids"]):
             rows[idx + j]["input_ids"] = pad_list_to_32(ids)
         check_sum = True
         texts = []
 tokens = tokenizer.batch_encode_plus(texts)
-for j, ids in enumerate(tokens['input_ids']):
+for j, ids in enumerate(tokens["input_ids"]):
     rows[idx + j]["input_ids"] = ids
 
 with open("dataset/koraddr_dataset_v3.pk", "wb") as f:
--- test.py
+++ test.py
@@ -3,53 +3,49 @@ import csv
 import unittest
 from koraddr.interface import KoreanAddress
 
+
 class KoreanAddressTests(unittest.TestCase):
     def load_dataset(self):
-        with open('dataset/road_address.csv', 'r') as csvfile:
+        with open("dataset/road_address.csv", "r") as csvfile:
             reader = csv.DictReader(csvfile)
             for row in reader:
                 self.road_address.append(row)
-        
-        with open('dataset/jibun_address.csv', 'r') as csvfile:
+
+        with open("dataset/jibun_address.csv", "r") as csvfile:
             reader = csv.DictReader(csvfile)
             for row in reader:
                 self.jibun_address.append(row)
-        
-        
+
     def setUp(self):
-        self.model = KoreanAddress("model/2024_04_03", use_gpu=False)
+        self.model = KoreanAddress("dataset/ensemble_model_v1.joblib")
         self.jibun_address = []
         self.road_address = []
         self.load_dataset()
 
-
     def test_predict_jibun_address(self):
         for row in self.jibun_address:
-            predict = self.model.predict(row['text'])
-            print(row['address_1'], "/", predict)
+            predict = self.model.predict(row["text"])
+            # print(row['address_1'], "/", predict)
             # assert row['address_1'] == predict
 
+    # def test_predict_road_address(self):
+    #     for row in self.road_address:
+    #         predict = self.model.predict(row['text'])
+    #         print(row['address_1'], "/", predict)
+    #         # assert row['address_1'] == predict
 
-    def test_predict_road_address(self):
-        for row in self.road_address:
-            predict = self.model.predict(row['text'])
-            print(row['address_1'], "/", predict)
-            # assert row['address_1'] == predict
+    # def test_deeper_predict_jibun_address(self):
+    #     for row in self.jibun_address:
+    #         predict = self.model.deeper_predict(row['text'])
+    #         print(row['address_2'], "/", predict)
+    #         # assert row['address_2'] == predict
 
-
-    def test_deeper_predict_jibun_address(self):
-        for row in self.jibun_address:
-            predict = self.model.deeper_predict(row['text'])
-            print(row['address_2'], "/", predict)
-            # assert row['address_2'] == predict
-
-
-    def test_deeper_predict_road_address(self):
-        for row in self.road_address:
-            predict = self.model.deeper_predict(row['text'])
-            print(row['address_2'], "/", predict)
-            # assert row['address_2'] == predict
+    # def test_deeper_predict_road_address(self):
+    #     for row in self.road_address:
+    #         predict = self.model.deeper_predict(row['text'])
+    #         print(row['address_2'], "/", predict)
+    #         # assert row['address_2'] == predict
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
